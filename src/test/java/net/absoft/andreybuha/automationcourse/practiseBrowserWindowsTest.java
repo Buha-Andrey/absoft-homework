35d09aa1ed8c7b0c4af278185d3edac2c939509b
andrey.buha <andrey.buha@ab-soft.net>
2023-03-09 14:00:24 +0200

    form and new window tasks

---
+++ src/test/java/net/absoft/andreybuha/automationcourse/practiseBrowserWindowsTest.java
@@ -0,0 +1,44 @@
+package net.absoft.andreybuha.automationcourse;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class practiseBrowserWindowsTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions().addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(options);
+        driver.get("https://demoqa.com/browser-windows");
+    }
+
+
+    @Test
+    public void testBrowserWindows() {
+        String windowHandle = driver.getWindowHandle();
+        driver.findElement(By.id("tabButton")).click();
+
+        String windowSample = driver.getWindowHandles().stream().filter(handle -> !handle.equals(windowHandle))
+                .findFirst()
+                .orElseThrow(() -> new AssertionError("New windows handle was not added"));
+
+        driver.switchTo().window(windowSample);
+
+        assertEquals(driver.findElement(By.id("sampleHeading")).getText(), "This is a sample page", "Test is failed");
+        driver.switchTo().window(windowHandle);
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+       driver.quit();
+    }
+}
---
+++ src/test/java/net/absoft/andreybuha/automationcourse/practiseFormTest.java
@@ -0,0 +1,76 @@
+package net.absoft.andreybuha.automationcourse;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.JavascriptExecutor;
+
+import java.time.Duration;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+
+public class practiseFormTest {
+
+    private WebDriver driver;
+    @BeforeMethod
+    private void setUp(){
+        ChromeOptions options = new ChromeOptions().addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(options);
+        driver.get("https://demoqa.com/automation-practice-form");
+
+   }
+    @Test
+    public void fillTheForm(){
+        WebElement moveToCheckboxes = driver.findElement(By.id("state"));
+        WebElement moveToSubmit = driver.findElement(By.xpath("//div[@class = 'accordion']/div[5]"));
+        WebElement selectDropdown = driver.findElement(By.id("state"));
+        WebElement selectCity = driver.findElement(By.id("city"));
+
+
+
+        driver.findElement(By.id("firstName")).sendKeys("RingCentral");
+        driver.findElement(By.id("lastName")).sendKeys("RingCentral-test");
+        driver.findElement(By.id("userEmail")).sendKeys("gwtest@test.com");
+        driver.findElement(By.xpath("//form//input[@id='gender-radio-1']/../label")).click();
+        driver.findElement(By.xpath("//form//input[@id='userNumber']")).sendKeys("1234567890");
+    /*
+           WebElement birthField = driver.findElement(By.id("dateOfBirthInput"));
+           birthField.clear();
+           birthField.sendKeys("02/06/1982");
+    */
+        driver.findElement(By.id("subjectsInput")).sendKeys("English");
+        driver.findElement(By.id("subjectsInput")).sendKeys(Keys.RETURN);
+        new Actions(driver).moveToElement(moveToCheckboxes).perform();
+        driver.findElement(By.xpath("//label[@for = 'hobbies-checkbox-1']")).click();
+        driver.findElement(By.id("uploadPicture")).sendKeys("/Users/andrey.buha/Desktop/Screenshot 2023-02-07 at 10.55.46.png");
+        driver.findElement(By.id("currentAddress")).sendKeys("Odesa, Ukraine");
+        driver.findElement(By.xpath("//div[@class = 'accordion']/div[4]")).click(); //need to get access to field with dropdown
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        new Actions(driver).moveToElement(moveToSubmit).perform();
+
+        new Actions(driver).click(selectDropdown).sendKeys(Keys.RETURN).perform();
+        new Actions(driver).click(selectCity).sendKeys(Keys.RETURN).perform();
+
+
+
+        driver.findElement(By.id("userForm")).submit();
+        assertEquals(driver.findElement(By.id("example-modal-sizes-title-lg")).getText(), "Thanks for submitting the form", "Test is failed");
+
+
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+            driver.quit();
+    }
+}
